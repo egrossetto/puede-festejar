85a0f1706f37e64d8a1a6fcb4904ade979ed671f
egrossetto <eugerosso22@hotmail.com>
2022-12-19 19:19:11 -0300

    puede festejar changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [username, setUsername] = useState('');
+
+	const buttonHandler = () => {
+		window.location.href = `https://twitter.com/search?q=from%3A${username}%20%20"messi"%20OR%20"scaloni"%20OR%20"scaloneta"%20OR%20"di maria"%20OR%20"de paul"%20&src=typed_query&f=top`;
+	};
+
+	return (
+		<div className="App">
+			<header>
+				<h2>Puede festear?</h2>
+			</header>
+			<main>
+				<h3>Ingrese su username:</h3>
+				<input
+					placeholder="@"
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<button onClick={buttonHandler}>Checkear</button>
+			</main>
+		</div>
+	);
 }
 
 export default App;
